6e993e4fd56285c65ea5a1ef1ac3f5cba9b3af99
Trunggenk <22028170@vnu.edu.vn>
2024-02-27 10:52:37 +0700

    1

---
+++ Strings.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Trung on 2/27/2024.
+//
+#include "all.h";
+using namespace std;
+int main()
+
+{
+    string a,b,a1,b1;
+    cin>>a;
+    cin.ignore();
+    cin>>b;
+    cout<< a.length()<<" "<<b.length()<<endl;
+    cout<<a+b<<endl;
+    char temp;
+    temp=a[0];
+    a[0]=b[0];
+    b[0]=temp;
+    cout<<a << " "<< b;
+}
