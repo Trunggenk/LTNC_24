d3c85183559d2cd43ef7e42d2e58d218466ac461
Trunggenk <22028170@vnu.edu.vn>
2024-03-04 16:20:23 +0700

    ch4

---
+++ Mystery Number.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Trung on 3/4/2024.
+//
+#include "all.h"
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int fa[101];
+    int b[n+1];
+    int fb[101];
+    for(int m=0; m<101; m++)
+    {
+        fa[m]=0;
+        fb[m]=0;
+    }
+    for(int i = 0 ; i< n ; i++) {
+        cin >> a[i];
+        fa[a[i]]++;
+    }
+    for(int j = 0 ; j<n+1 ; j++) {
+        cin >> b[j];
+        fb[b[j]]++;
+    }
+    for(int m=0; m<101; m++ )
+    {
+        if(fa[m]!=fb[m]) {
+            cout << m;
+            return 0;
+        }
+    }
+}
