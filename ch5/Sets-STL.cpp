3e45b5d4791334c4bcf035807c01d0d2dd5f120f
Trung <22028170@vnu.edu.vn>
2024-03-25 07:53:47 +0700

    ch5

--- ch5/Sets-STL.cpp
+++ ch5/Sets-STL.cpp
@@ -1,3 +1,47 @@
-//
-// Created by trung on 3/14/2024.
-//
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+ set<int> s;
+ int q;
+ cin>>q;
+ for(int i =1 ; i<= q; i++)
+ {
+     int x;
+     int y;
+     cin>>x>>y;
+     if(x==1)
+     {
+         s.insert(y);
+         }
+     if(x==2)
+     {
+         s.erase(y);
+     }
+     if(x==3) {
+         set<int>::iterator itr = s.find(y);
+         if(itr != s.end())
+         {
+             cout<<"YES";
+         }
+         else
+         if( *itr == y)
+         cout<<"YES";
+         else
+             if(*itr != y)
+             {
+                 cout<<"NO";
+             }
+
+     }
+
+ }
+}
+
+
