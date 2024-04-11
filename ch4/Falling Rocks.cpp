15bf4c37adb4ae10f64d07fe5f02d01846f00363
Trung <22028170@vnu.edu.vn>
2024-04-11 16:09:05 +0700

    ch6

--- ch4/Falling Rocks.cpp
+++ ch4/Falling Rocks.cpp
@@ -1,6 +1,26 @@
-//
-// Created by Trung on 3/7/2024.
-//
+#include <vector>
+using namespace std;
 
-#include "../all.h"
+bool canSurvive(vector<vector<char>> matrix, int playerPos) {
+    int H = matrix.size();
+    int W = matrix[0].size();
 
+    vector<vector<bool>> dp(H, vector<bool>(W, false));
+    dp[H-1][playerPos] = true;
+
+    for (int i = H - 1; i > 0; --i) {
+        for (int j = 0; j < W; ++j) {
+            if (dp[i][j] && matrix[i][j] != 'R') {
+                dp[i-1][j] = true;
+                if (j > 0) dp[i-1][j-1] = true;
+                if (j < W - 1) dp[i-1][j+1] = true;
+            }
+        }
+    }
+
+    for (int j = 0; j < W; ++j) {
+        if (dp[0][j]) return true;
+    }
+
+    return false;
+}
--- ch6/Box It.cpp
+++ ch6/Box It.cpp
@@ -4,84 +4,57 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-//Implement the class Box
-//l,b,h are integers representing the dimensions of the box
 
-// The class should have the following functions :
-
-// Constructors:
-// Box();
-// Box(int,int,int);
-// Box(Box);
-
-
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
-class Box{
+class Box {
 public:
-    int l,b,h;
-//// Constructors:
-    Box(){
-        l = 0;
-        b = 0;
-        h = 0;
-    }
-    Box(int l, int b, int h){
-        this->l = l;
-        this->b = b;
-        this->h = h;
-    }
-    Box(Box &B){
-        l = B.l;
-        b = B.b;
-        h = B.h;
-    }
-    int getLength(){
-        return l;
-    }
-    int getBreadth(){
-        return b;
-    }
-    int getHeight(){
-        return h;
-    }
-    long long CalculateVolume(){
-        return (long long)l*b*h;
-    }
-    bool operator<(Box &B){
-        if(l < B.l || (b < B.b && l == B.l) || (h < B.h && b == B.b && l == B.l)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    friend ostream& operator<<(ostream& out, Box &B){
-        out<<B.l<<" "<<B.b<<" "<<B.h;
-        return out;
-    }
-//// int getLength(); // Return box's length
-//// int getBreadth (); // Return box's breadth
-//// int getHeight ();  //Return box's height
-//// long long CalculateVolume(); // Return the volume of the box
-////
-//// //Overload operator < as specified
-//// bool operator<(Box& b)
-////
-//// //Overload operator << as specified
-//// ostream& operator<<(ostream& out, Box& B)
-//// Box();
-//// Box(int,int,int);
-//// Box(Box);
-
+int l;
+int b;
+int h;
+Box(){
+    l=0;
+    b=0;
+    h=0;
+};
+Box(int length, int breadth, int height)
+{
+    l=length;
+    b=breadth;
+    h=height;
+};
+Box(Box &B)
+{
+    l=B.l;
+    b=B.b;
+    h=B.h;
+};
+int getLength()
+{
+    return l;
+}
+int getBreadth()
+{
+    return b;
+}
+int getHeight()
+{
+    return h;
+}
+long long CalculateVolume()
+{
+    return (long long)l*b*h;
+}
+bool operator<(Box& B)
+{
+    if(l<B.l) return true;
+    else if(b<B.b && l==B.l) return true;
+    else if(h<B.h && b==B.b && l==B.l) return true;
+    else return false;
+}
+friend ostream& operator<<(ostream& out, Box& B)
+{
+    out<<B.l<<" "<<B.b<<" "<<B.h;
+    return out;
+}
 };
 
 void check2()
@@ -99,7 +72,7 @@ void check2()
         }
         if(type == 2)
         {
-            int l,b,h;
+            int l,b,h=0;
             cin>>l>>b>>h;
             Box NewBox(l,b,h);
             temp=NewBox;
