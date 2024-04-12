bece825a269007d279c68794aaf84ddf7ec82eca
Trung <22028170@vnu.edu.vn>
2024-04-12 11:45:40 +0700

    ch6

--- ch6/Box It.cpp
+++ ch6/Box It.cpp
@@ -1,60 +1,68 @@
-//
-// Created by trung on 4/8/2024.
-//
 #include<bits/stdc++.h>
 
 using namespace std;
+//Implement the class Box
+//l,b,h are integers representing the dimensions of the box
 
+// The class should have the following functions :
+
+// Constructors:
+// Box();
+// Box(int,int,int);
+// Box(Box);
+
+
+// int getLength(); // Return box's length
+// int getBreadth (); // Return box's breadth
+// int getHeight ();  //Return box's height
+// long long CalculateVolume(); // Return the volume of the box
+
+//Overload operator < as specified
+//bool operator<(Box& b)
+
+//Overload operator << as specified
+//ostream& operator<<(ostream& out, Box& B)
 class Box {
 public:
-int l;
-int b;
-int h;
-Box(){
-    l=0;
-    b=0;
-    h=0;
-};
-Box(int length, int breadth, int height)
-{
-    l=length;
-    b=breadth;
-    h=height;
-};
-Box(Box &B)
-{
-    l=B.l;
-    b=B.b;
-    h=B.h;
-};
-int getLength()
-{
-    return l;
-}
-int getBreadth()
-{
-    return b;
-}
-int getHeight()
-{
-    return h;
-}
-long long CalculateVolume()
-{
-    return (long long)l*b*h;
-}
-bool operator<(Box& B)
-{
-    if(l<B.l) return true;
-    else if(b<B.b && l==B.l) return true;
-    else if(h<B.h && b==B.b && l==B.l) return true;
-    else return false;
-}
-friend ostream& operator<<(ostream& out, Box& B)
-{
-    out<<B.l<<" "<<B.b<<" "<<B.h;
-    return out;
-}
+    int l;
+    int b;
+     int h;
+   Box(){
+        l=0;
+        b=0;
+        h=0;
+
+   }
+   Box(int l , int b, int h )
+   {
+       Box::l=l;
+       Box::b = b;
+       Box:: h = h;
+   }
+
+   int getLength() const
+   {
+       return l;
+   }
+   int getBreath() const
+   {
+       return b;
+   }
+   int getheight() const
+   {
+       return h;
+   }
+   long long CalculateVolume(){
+       return (long long) l*b*h;
+   }
+    bool operator<( Box& other)  {
+
+        return this->CalculateVolume() < other.CalculateVolume();
+    }
+    friend ostream& operator<<(ostream& out, const Box& B) {
+        out << B.l << " " << B.b << " " << B.h;
+        return out;
+    }
 };
 
 void check2()
@@ -72,7 +80,7 @@ void check2()
         }
         if(type == 2)
         {
-            int l,b,h=0;
+            int l,b,h;
             cin>>l>>b>>h;
             Box NewBox(l,b,h);
             temp=NewBox;
